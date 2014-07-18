805268dae738996928e2c49277f07d085d4789d5
nnovak13 <nnovak@fordham.edu>
2014-07-18 09:07:59 -0400

    User can click on create button and receive an alert message

--- app/assets/javascripts/app_ready.js
+++ app/assets/javascripts/app_ready.js
@@ -1,3 +1,25 @@
-$(document).ready(function() {
+var ToDoApp = {
+  initialize: function() {
+    $('#da-Golden-Snitch').click(function(event) {
+      alert('You did it!');
+      event.preventDefault();
+    });
 
-});
+    // $('#disorient').click(this.mirrorText);
+    // $('#random-color-button, .color-changing').click(this.randomizeColor);
+    // $('#word-form').submit(this.addFunnyWord);
+    // $('#words-list').on('click', 'li', this.randomizeColor);
+    // $('#randomize-words-button').click($.proxy(this.randomizeWords, this));
+  },
+
+  // mirrorText: function() {
+  //   $(this).toggleClass('mirror');
+
+};
+// $(document).ready(function() {
+
+// });
+
+$(document).ready(function(){ ToDoApp.initialize(); });
+
+// In the $(document).ready() handler in app_ready.js, add a click event handler to your "Create" button that calls a function on TodoApp, which will create the item from the text in the form field
